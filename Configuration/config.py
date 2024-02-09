6e1d8f78001a932c417e8b27249da64a4e967082
Viacheslav Postolatii <postolatiivv@gmail.com>
2024-02-09 11:22:43 +0200

    Committing changes to default_file_initializer.py in the savio-1.0 branch

--- Configuration/config.py
+++ Configuration/config.py
@@ -1,3 +1,7 @@
 """Config file with consts or paths."""
 
-PATH_TO_JSON_FILE = "Tools/data.json"
+BASE_DIR = "/home/savio/University/own_education/python/python_with_Maria/Project_1/"
+PATH_TO_JSON_FILE = BASE_DIR + "ProductsStore/Tools/data.json"
+PATH_TO_TEST_JSON_FILE = BASE_DIR + "ProductsStore/Tools/TestData/test_data.json"
+PATH_TO_LOG_FILE = BASE_DIR + "ProductsStore/Logs/logs.log"
+
