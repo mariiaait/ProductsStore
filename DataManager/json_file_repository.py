0340991c5519f5082605d8cdcc5cac22f02da703
djygan <djyganyr@gmail.com>
2024-02-11 22:35:28 +0200

    created delete_method

--- DataManager/json_file_repository.py
+++ DataManager/json_file_repository.py
@@ -1,12 +1,32 @@
 """CRUD operations. Executing of request only. If request throw an exception,
 the functions in the 'json_file_service' will handle it."""
 import json
+from Configuration.config import PRODUCT_ID, PRODUCTS, INDENT
+
 
 def add(path: str, data: dict) -> None:
     """Represents the logic of adding data to file.
      path - this is the path to the file with basic data for CRUD.
      data - data to add."""
-    pass
+    all_data = get(path)
+    if not is_exists(all_data, data):
+        all_data[PRODUCTS].append(data)
+        write(path, all_data)
+
+
+def is_exists(data: dict, all_data: dict):
+    """Checks for existing data in all_data
+    data - this is input data for checking
+    all_data - this is data from the file data.json"""
+    return any(map(lambda item: item[PRODUCT_ID] == data[PRODUCT_ID], all_data))
+
+
+def write(path: str, data: dict):
+    """Writes data to a JSON file
+    path - this is the path to the file with basic data for CRUD
+    data - data to write to a file"""
+    with open(path, 'w') as file:
+        json.dump(data, file, indent=INDENT)
 
 
 def get(path: str) -> dict[list[dict]]:
@@ -27,7 +47,11 @@ def delete(path: str, id: int) -> None:
     """Represents the logic of deleting data from file by id.
          path - this is the path to the file with basic data for CRUD.
          id - this is id of data."""
-    pass
+    all_data = get(path)
+    target = list(item for item in all_data[PRODUCTS] if item[PRODUCT_ID] == id)
+    if target:
+        all_data[PRODUCTS].remove(*target)
+        write(path, all_data)
 
 
 def update(path: str, data: dict) -> None:
