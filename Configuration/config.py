a765b59a4259560e63a21e8d2d6ae406dc20548d
Viacheslav Postolatii <postolatiivv@gmail.com>
2024-02-11 15:52:51 +0200

    created initialize method, filled the JSON data_json file with data from test_data.json file

--- Configuration/config.py
+++ Configuration/config.py
@@ -1,6 +1,7 @@
 """Config file with consts or paths."""
 
-PATH_TO_JSON_FILE = "Tools/data.json"
+PATH_TO_JSON_FILE = "Tools/data1.json"
+PATH_TO_TEST_JSON_FILE = "Tools/TestData/test_data.json"
 DEFAULT_FILE_DATA = {"products": []}
 PRODUCT_ID = 'id'
 PRODUCT_NAME = 'name'
