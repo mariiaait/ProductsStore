db106a01499136f6184c1892176875215ecb050b
Viacheslav Postolatii <postolatiivv@gmail.com>
2024-02-13 19:27:21 +0200

    add to dict function

--- Infrastructure/Converters/to_dict.py
+++ Infrastructure/Converters/to_dict.py
@@ -1,7 +1,13 @@
 """File which responsible for converting passed data to dict type."""
 
+from Configuration.config import PRODUCT_ID, PRODUCT_NAME
+
 
 def to_dict(*args: tuple) -> dict:
     """Function which converts passed data to dictionary.
     *args - data to convert."""
-    pass
+    if len(args) == 2:
+        id, name = args
+        return {PRODUCT_ID: id, PRODUCT_NAME: name}
+    else:
+        raise ValueError("There are not two arguments")
