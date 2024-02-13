1bc9032fdf3aa8167af0823166f58a7b27e822f8
Viacheslav Postolatii <postolatiivv@gmail.com>
2024-02-13 18:49:03 +0200

    add return

--- BusinessLogic/json_file_service.py
+++ BusinessLogic/json_file_service.py
@@ -8,27 +8,27 @@ def add(path: str, *args: tuple) -> bool:
     path - this is the path to the file with basic data for CRUD.
     args - data which adds to the file by path."""
     data = None
-    repository.add(PATH_TO_JSON_FILE, data)
+    return repository.add(PATH_TO_JSON_FILE, data)
 
 
 def get(path: str) -> dict[list[dict]]:
     """Business logic of getting data from file.
        path - this is the path to the file with basic data for CRUD."""
-    repository.get(PATH_TO_JSON_FILE)
+    return repository.get(PATH_TO_JSON_FILE)
 
 
 def get_by_id(path: str, id: int) -> dict:
     """Business logic of getting data from file by id.
          path - this is the path to the file with basic data for CRUD.
          id - this is id of data."""
-    repository.get_by_id(PATH_TO_JSON_FILE, PRODUCT_ID)
+    return repository.get_by_id(PATH_TO_JSON_FILE, PRODUCT_ID)
 
 
 def delete(path: str, id: int) -> bool:
     """Business logic of deleting data from file by id.
          path - this is the path to the file with basic data for CRUD.
          id - this is id of data."""
-    repository.delete(PATH_TO_JSON_FILE, PRODUCT_ID)
+    return repository.delete(PATH_TO_JSON_FILE, PRODUCT_ID)
 
 
 def update(path: str, *args: tuple) -> bool:
@@ -36,10 +36,10 @@ def update(path: str, *args: tuple) -> bool:
              path - this is the path to the file with basic data for CRUD.
              args - data to update."""
     data = None
-    repository.update(PATH_TO_JSON_FILE, data)
+    return repository.update(PATH_TO_JSON_FILE, data)
 
 
-def get_last() -> dict:
+def get_last(path: str) -> dict:
     """Business logic of getting last data from file.
           path - this is the path to the file with basic data for CRUD."""
-    repository.get()
+    return repository.get_last()
