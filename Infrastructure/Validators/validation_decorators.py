d269db6772faa45e24aa9764e0e55b44d37bd0cc
Viacheslav Postolatii <postolatiivv@gmail.com>
2024-02-16 12:06:41 +0200

    create the handle_crud_exceptions decorator

--- Infrastructure/Validators/validation_decorators.py
+++ Infrastructure/Validators/validation_decorators.py
@@ -1,9 +1,11 @@
 """Decorators of validation for json_file_service file."""
 from functools import wraps
 from typing import get_type_hints, Callable
+import logging
+from Configuration.config import PATH_TO_LOG_FILE
 
 
-def check_parameters_types(func)->Callable:
+def check_parameters_types(func) -> Callable:
     @wraps(func)
     def wrapper(*args, **kwargs):
         type_hints = get_type_hints(func)
@@ -13,7 +15,27 @@ def check_parameters_types(func)->Callable:
         for name, value in pairs:
             type_hint = type_hints[name]
             if not isinstance(value, type_hint):
-                raise TypeError (f'Expected {type_hint}. Got {type(value)}')
+                raise TypeError(f'Expected {type_hint}. Got {type(value)}')
         return func(*args, **kwargs)
 
     return wrapper
+
+
+logging.basicConfig(filename=PATH_TO_LOG_FILE, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def handle_crud_exceptions(log_mes: str) -> Callable:
+    def check_exception(func) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                logging.info(log_mes)
+                response = func(*args, **kwargs)
+                return {"is_success": True, "response": response, "errors": None}
+            except Exception as ex:
+                logging.error(ex)
+                return {"is_success": False, "response": None, "errors": str(ex)}
+
+        return wrapper
+
+    return check_exception
